6497d077c1b45c133984848b8df5f60056187969
2111805214 <48742661+2111805214@users.noreply.github.com>
2021-01-07 17:49:57 +0800

    Add files via upload

--- knn.m
+++ knn.m
@@ -3,15 +3,10 @@
     matrix = P;
     A = zeros(n,1);
     knn_network = zeros(n, n);
-    P= P-diag(diag(P)); 
-   
     [sort_network,idx]=sort(P,2,'descend');
     for i = 1 : n
-        A(i)=sum(sort_network(i,1:k));
-        knn_network(i,idx(i,1:k))=sort_network(i,1:k);
-        %knn_network(i,idx(i,1:k)) = exp(-(norm(matrix(i,:) - matrix(idx(i,1:k),:)).^2) / 4);
-        knn_network(i,:) = knn_network(i,:) ./ A(i);
-        
+       knn_network(i,idx(i,1:k))=sort_network(i,1:k);
+       knn_network(i,:) = knn_network(i,:) ./ A(i);
     end
  
 end
